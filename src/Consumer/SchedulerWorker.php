2f433fb21615617fd34570bddf7c61beed06cbdf
monky <yanbo@luojilab.com>
2016-11-18 17:49:50 +0800

    fix the SchedulerWorker

--- src/Consumer/SchedulerWorker.php
+++ src/Consumer/SchedulerWorker.php
@@ -9,7 +9,7 @@ class SchedulerWorker{
 
     private $process;
     private $queue;
-    private $task_prefix = 'ebats_core_';
+    private $task_prefix = 'ebats_task_';
 
     public function __construct($process) {
         $this->process = $process;
@@ -22,7 +22,7 @@ class SchedulerWorker{
     public function run(){
         $worker = new Worker($this->process);
         $worker->setExchange(Scheduler::EXCHANGE_EVENT);
-        $worker->setQueue($this->task_prefix.$this->queue, $this->queue);
+        $worker->setQueue($this->task_prefix.$this->queue, [$this->queue]);
         $worker->start();
     }
 }
