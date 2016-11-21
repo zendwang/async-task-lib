e2571baa828a41e2048f8603878a3da1a1cfcda6
monky <yanbo@luojilab.com>
2016-11-21 19:06:05 +0800

    增加进程数控制

--- src/Consumer/SchedulerWorker.php
+++ src/Consumer/SchedulerWorker.php
@@ -8,11 +8,13 @@ namespace Asynclib\Consumer;
 class SchedulerWorker{
 
     private $process;
+    private $process_num;
     private $queue;
     private $task_prefix = 'ebats_task_';
 
-    public function __construct($process) {
+    public function __construct($process, $process_num) {
         $this->process = $process;
+        $this->process_num = $process_num;
     }
 
     public function setQueue($name) {
@@ -20,7 +22,7 @@ class SchedulerWorker{
     }
 
     public function run(){
-        $worker = new Worker($this->process);
+        $worker = new Worker($this->process, $this->process_num);
         $worker->setExchange(Scheduler::EXCHANGE_TASK);
         $worker->setQueue($this->task_prefix.$this->queue, [$this->queue]);
         $worker->start();
